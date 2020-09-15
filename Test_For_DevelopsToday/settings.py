243036c273590721fe27c2830566cfdf03104dce
Unlimit78 <taras_ilyashchuk161@ukr.net>
2020-09-15 14:56:06 +0300

    deploy

--- Test_For_DevelopsToday/settings.py
+++ Test_For_DevelopsToday/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "!&(9ya5zj$gf4n0+8&pwgfjvd51k4)g+g1-do1f7n=bsxi2834"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -121,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CELERY_BROKER_URL = "redis://h:pfe15eb49845735864840260ebf90495e88f6c5fddba9d719ee09885651fcb359@ec2-54-197-140-154.compute-1.amazonaws.com:23989"
 
